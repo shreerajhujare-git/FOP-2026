8556ea0ff60c29819da0285ad1b4f7a5dabf070b
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-02-05 08:29:51 +0000

    Add matrix.c file with dynamic matrix allocation based on user input

---
+++ Classwork/matrix.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int rows, cols, i, j;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    int matrix[rows][cols];
+   
+}

5288fbf6aa4962bf17d54b389387635fc666aaef
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-02-06 03:22:55 +0000

    Add multiplication table program to Assignment03.c

--- Assignments/Assignment03.c
+++ Assignments/Assignment03.c
@@ -5,3 +5,13 @@ void main () {
     // Program to print multiplication table of a given number
 
     int num, i;
+    
+printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Multiplication table of %d:\n", num);
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+}

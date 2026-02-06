1524fcbdb5cfb10e6168ad212ecc494147e957a0
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-02-06 03:25:50 +0000

    Refactor salary calculation program for clarity and structure

--- Assignments/Assignment04.c
+++ Assignments/Assignment04.c
@@ -9,3 +9,16 @@ void main () {
     // Profession Tax = 2% of total salary
 
     float basicPay, hra, ta, grossSalary, professionTax, netSalary;
+
+    printf("Basic Pay: ");
+    scanf("%f", &basicPay);
+
+    hra = 0.10 * basicPay;
+    ta = 0.05 * basicPay;
+    grossSalary = basicPay + hra + ta;
+    professionTax = 0.02 * grossSalary;
+    netSalary = grossSalary - professionTax;
+    
+    printf("Gross Salary: %.2f\n", grossSalary);
+    printf("Net Salary: %.2f\n", netSalary);
+}

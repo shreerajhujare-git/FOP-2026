733a28749d55ebf35c856ca00cf49e6440c4cbcf
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-02-06 03:25:05 +0000

    Add program to calculate employee salary based on basic pay

---
+++ Assignments/Assignment04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main () {
+
+    // Assignment 04
+    // Program to calculate salary of an employee given basic pay
+    // Calculate gross salary and net salary
+    // HRA = 10% of basic pay
+    // TA = 5% of basic pay
+    // Profession Tax = 2% of total salary
+
+    float basicPay, hra, ta, grossSalary, professionTax, netSalary;

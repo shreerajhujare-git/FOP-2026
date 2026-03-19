a975b7d673099c98afefcb664a7a79d71fe11130
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-03-19 18:12:57 +0000

    Add program to calculate income tax based on user input

---
+++ Udemy_course/tax_calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float income, tax = 0;
+
+    printf("Enter your income: ");
+    scanf("%f", &income);
+
+    if (income <= 250000) {
+        tax = 0;
+         } 
+    else if (income <= 500000) {
+        tax = (income - 250000) * 0.05;
+    } 
+    else if (income <= 1000000) {
+        tax = (250000 * 0.05) + (income - 500000) * 0.20;
+    } 
+    else {
+        tax = (250000 * 0.05) + (500000 * 0.20) + (income - 1000000) * 0.30;
+    }
+
+    printf("Tax to be paid = %.2f\n", tax);
+
+    return 0;
+}

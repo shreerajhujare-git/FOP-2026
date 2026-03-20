5fc07749ba3cb35d0eeddb9d3a820e352a710433
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-03-20 05:52:24 +0000

    New Assignment: Assignment14.c

---
+++ Assignments/Assignment14.c
@@ -0,0 +1,17 @@
+// this program is to convert binary into decimal number system
+#include <stdio.h>
+#include <math.h>
+int main() {
+   
+    int binary, decimal = 0, rem, i = 0;
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);
+    while (binary != 0) {
+        rem = binary % 10;
+        decimal += rem * pow(2, i);
+        binary /= 10;
+        i++;
+    }
+    printf("Decimal equivalent: %d\n", decimal);
+    return 0;
+}

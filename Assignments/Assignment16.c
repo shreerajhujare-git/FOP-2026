72155bf9ff8728a7bb6ead1b979e9fd71a072da7
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-03-26 14:02:34 +0000

    this program is to accept a number from the user and reverse it

--- Assignments/Assignment16.c
+++ Assignments/Assignment16.c
@@ -1,4 +1,4 @@
 // This program will accept list of N integers and partition list into two sublists even and odd numbers. Then it will display the two sublists.        
 #include <stdio.h>
 #include <stdlib.h> 
-int main() 
+int main()  

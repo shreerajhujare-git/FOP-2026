bb8d33f0ed837aac378b52e2524baa87d6907575
shreerajhujare-git <shreeraj.hujare@gmail.com>
2026-03-26 05:28:13 +0000

    this program is to accept a number from the user and reverse it.

---
+++ Assignments/Assignment13.c
@@ -0,0 +1,9 @@
+// this program is to accept a number and reverse it.
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0, remainder;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
